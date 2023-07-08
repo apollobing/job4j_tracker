545015340a56c773c0d8f47c942e763457d40f29
apollobing <taras.career@gmail.com>
2023-07-08 16:02:24 +0200

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Cat " + this.name + " ate " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        peppy.giveNick("Pep");
+        peppy.eat("Milk");
+        peppy.show();
+        sparky.giveNick("Spar");
+        sparky.eat("Popcorn");
+        sparky.show();
     }
 }
