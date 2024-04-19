afde7279e6bc9ae42bb8cb4d72238ee550e12897
apollobing <taras.career@gmail.com>
2024-04-19 15:00:53 +0200

    2. @Setter, @Getter и другие [#504889]

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,16 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+    @EqualsAndHashCode.Include
+    @NonNull
+    @Getter
+    private int id;
+
+    @Getter
+    @Setter
+    private String name;
+}
